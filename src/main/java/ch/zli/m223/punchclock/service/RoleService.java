26b55f46b7ae483b35a4eb792a06e7a9147eeae7
kricon <raynohd@gmail.com>
2021-07-16 07:21:00 -0700

    1. New Dashboard for Admin 2. In Dashboard Admin can Create, Edit and Delere Users 3. Now working on Roles

--- src/main/java/ch/zli/m223/punchclock/service/RoleService.java
+++ src/main/java/ch/zli/m223/punchclock/service/RoleService.java
@@ -5,12 +5,12 @@ import ch.zli.m223.punchclock.domain.Entry;
 import ch.zli.m223.punchclock.domain.Role;
 import ch.zli.m223.punchclock.repository.ApplicationUserRepository;
 import ch.zli.m223.punchclock.repository.RoleRepository;
-import org.jvnet.hk2.annotations.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
